d6b96162498de743d805656657a69d71d0decee9
Matt <matt.dignum@googlemail.com>
2020-11-13 12:31:12 +0000

    Remove unnessecary usings

--- Source/Service/ITransactionEventProcessor.cs
+++ Source/Service/ITransactionEventProcessor.cs
@@ -1,6 +1,4 @@
-﻿using System.Threading.Tasks;
-
-namespace Service
+﻿namespace Service
 {
     public interface ITransactionEventProcessor
     {
--- Tests/Service.Tests/TransactionEventProcessorTests.cs
+++ Tests/Service.Tests/TransactionEventProcessorTests.cs
@@ -7,7 +7,6 @@ using Service.Messaging;
 using Service.StoreMessages.Enums;
 using System;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Service.Tests
